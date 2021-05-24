e221499edcf7d891b9c2a77a3ed4715479df46db
Vijay Kumar Rajput <vijay.kumar1@sony.com>
2021-05-23 23:56:01 -0700

    Read the file

--- src/main/java/com/vijay/vocabulary/Vocabulary.java
+++ src/main/java/com/vijay/vocabulary/Vocabulary.java
@@ -1,10 +1,11 @@
 package com.vijay.vocabulary;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Stream;
 
 /**
  * @author vkumar1 created on 5/23/21
@@ -13,21 +14,61 @@ public class Vocabulary {
 
     public static void main(String[] args) {
         Vocabulary vocabulary = new Vocabulary();
-
-        List<Word> words = vocabulary.readFile("words.txt");
+        InputStream inputStream = vocabulary.getFileFromResourceAsStream("words.txt");
+        List<Word> words = vocabulary.readFile(inputStream);
         System.out.println(words);
-
     }
 
 
-    private List<Word> readFile(String fileName) {
+    private List<Word> readFile(InputStream inputStream) {
         List<Word> words = new LinkedList<>();
-        try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+        try (InputStreamReader inputStreamReader =
+                     new InputStreamReader(inputStream, "UTF-8");
+             BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
+            String line = null;
+            Word word = null;
+            boolean newWord = false;
+            int lineNo = 0;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+                if (!line.contains(" ")) {
+                    lineNo = 1;
+                    if (word != null) {
+                        words.add(word);
+                    }
+                    word = new Word();
+                    word.setWord(line);
+                } else if (lineNo == 1) {
+                    word.setMeaning(line);
+                    lineNo = 2;
+                } else if (lineNo == 2) {
+                    word.setSentence(line);
+                    lineNo = 2;
+                }
 
+            }
 
-        } catch (Exception ex) {
-            System.out.println(ex);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
         }
         return words;
+    }
+
+
+    // get a file from the resources folder
+    // works everywhere, IDEA, unit test and JAR file.
+    private InputStream getFileFromResourceAsStream(String fileName) {
+
+        // The class loader that loaded the class
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(fileName);
+
+        // the stream holding the file content
+        if (inputStream == null) {
+            throw new IllegalArgumentException("file not found! " + fileName);
+        } else {
+            return inputStream;
+        }
+
     }
 }
